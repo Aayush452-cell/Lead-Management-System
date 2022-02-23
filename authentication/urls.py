b4cd534c6f3eef0532bc515eb6f0ae701fdceb57
Aayush452-cell <malemeaayushkumarsingh@gmail.com>
2022-02-23 06:25:09 +0530

    register functionality added

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.user_login, name='login'),
+    path('signup/', views.user_register, name='user_register'),
     path('logout/', views.user_logout, name='logout')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,21 +1,39 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import CustomUser
 from django.http import HttpResponse
+
 
 def user_login(request):
     if request.method == 'POST':
-        username = request.POST['username']
+        email = request.POST['email']
         userpass = request.POST['password']
         try:
-            user_obj = authenticate(username=username, password=userpass)
+            user_obj = authenticate(email=email, password=userpass)
             login(request, user_obj)
-            request.session['username'] = username
+            request.session['email'] = email
             return HttpResponse("You are logged in !!!")
         except:
             return HttpResponse("Something went wrong")
     else:
         return render(request, 'login.html')
+
+
+def user_register(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        phone_no = request.POST['phone']
+        userpass = request.POST['password']
+        try:
+            user = CustomUser.objects.create_user(email=email, first_name=fname, last_name=lname, password=userpass,
+                                              phone_no=phone_no)
+            return redirect('login')
+        except:
+            return HttpResponse("Something went wrong")
+    return render(request, 'register.html')
+
 
 def user_logout(request):
     try:
