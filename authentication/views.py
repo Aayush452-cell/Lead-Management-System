2730d8489ba421c7abe104bd7055085bbe9b1676
Aayush452-cell <malemeaayushkumarsingh@gmail.com>
2022-03-11 19:03:21 +0530

    List Leads And Filter Implemented

--- authentication/views.py
+++ authentication/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .models import CustomUser
+from .models import CustomUser,Leads
 from django.http import HttpResponse
 
 
+
 def user_login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        userpass = request.POST['password']
-        try:
-            user_obj = authenticate(email=email, password=userpass)
-            login(request, user_obj)
-            request.session['email'] = email
-            return HttpResponse("You are logged in !!!")
-        except:
-            return HttpResponse("Something went wrong")
+    if request.user.is_authenticated:
+        leads = Leads.objects.all()
+        return render(request, 'User_list_page.html', {'leads': leads})
     else:
-        return render(request, 'User_list_page.html')
+        if request.method == 'POST':
+            email = request.POST['email']
+            userpass = request.POST['password']
+            try:
+                user_obj = authenticate(email=email, password=userpass)
+                login(request, user_obj)
+                request.session['email'] = email
+                leads = Leads.objects.all()
+                return render(request, 'User_list_page.html', {'leads': leads})
+            except:
+                return HttpResponse("Something went wrong")
+        else:
+            return render(request, 'login.html')
 
 
 def user_register(request):
@@ -38,6 +44,15 @@ def user_register(request):
 def user_logout(request):
     try:
         logout(request)
-        return HttpResponse("You are logged OUT !!!")
+        return redirect('login')
     except:
         return HttpResponse("Something Went wrong")
+
+
+def filtering(request, pk):
+    if request.method == 'GET':
+        leads = Leads.objects.filter(status=pk)
+        return render(request, 'User_list_page.html', {'leads': leads})
+    else:
+        return render(request, 'User_list_page.html')
+
