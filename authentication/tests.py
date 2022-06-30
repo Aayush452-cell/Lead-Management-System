5ee8a13d23aa502ed2b8d904b01c7ffcea436904
Aayush_000 <aayush.kumar@transorg.com>
2022-06-30 17:17:07 +0530

    Added configs for CI/CD

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import CustomUser,Leads
 
-# Create your tests here.
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email="email@gmail.com", first_name="email", last_name="singh",
+                                                 password="email@000")
+        self.lead = Leads.objects.create(name="name",email="lead@gmail.com",status="cold",notes="new lead",sales_representative=self.user)
+
+    def test_CustomUser_model(self):
+        user = self.user
+        self.assertTrue(isinstance(user, CustomUser))
+        self.assertEqual(str(user), 'email@gmail.com')
+
+    def test_Leads_model(self):
+        lead = self.lead
+        self.assertTrue(isinstance(lead, Leads))
+        self.assertEqual(str(lead), 'lead@gmail.com')
